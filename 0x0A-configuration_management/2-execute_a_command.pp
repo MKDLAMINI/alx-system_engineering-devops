dc56e87eeff0f23512c3c394f8ad4d37f6d85a0c
MKDLAMINI <dlaminimkultra@gmail.com>
2024-04-19 12:37:50 -0700

    Using Puppet, create a manifest that kills a process named killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# This code kills a process && works together with the killmenow file which has already been given
+exec { killmenow':
+  command   =>  'usr/bin/pkill killmenow',
+  provider  =>  'shell',
+  returns   =>  [0, 1],
+}
